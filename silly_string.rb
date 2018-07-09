9e1bded94076eb02e0f024feb700b8a8a28011d8
shannonbee <shannonbenyamin@gmail.com>
2018-07-09 22:16:00 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -13,7 +13,7 @@ puts "Shannon".upcase
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
-phrase = "IM NOT SHOUTING"
+phrase = "IM NOT SHOUTING".downcase
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
@@ -21,7 +21,7 @@ puts lowercased_phrase
 
 # Challenge 4: Count how many letters are in that big_word
 
-big_word = "supercalifragilisticexpialidocious"
+big_word = "supercalifragilisticexpialidocious".length
 
 letter_count = big_word # Put a . after big_word and use a String method.
 
